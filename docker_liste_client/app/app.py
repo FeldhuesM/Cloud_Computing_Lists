086f59d6f190f11ae008fa67ac9a772613c9b9a7
MFDoom <137491265+FeldhuesM@users.noreply.github.com>
2024-06-28 18:52:09 +0200

    von server zu 192.168.178.94:5001
    Änderung von http://server:5001 zu http://192.168.178.94:5001

--- docker_liste_client/app/app.py
+++ docker_liste_client/app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     selected_list = request.args.get('list', 'Einkaufszettel')
-    response = requests.get(f"http://server:5001/lists/{selected_list}")
+    response = requests.get(f"http://192.168.178.94:5001/lists/{selected_list}")
     data = response.json()
     return render_template("index.html", lists=data['lists'], current_list=data['current_list'], list_name=selected_list)
 
@@ -15,7 +15,7 @@ def home():
 def add_item():
     list_name = request.form['list_name']
     item = request.form['item']
-    response = requests.post("http://server:5001/add_item", json={'list_name': list_name, 'item': item})
+    response = requests.post("http://192.168.178.94:5001/add_item", json={'list_name': list_name, 'item': item})
     return redirect(url_for('home', list=list_name))
 
 
@@ -23,20 +23,20 @@ def add_item():
 def delete_item():
     list_name = request.form['list_name']
     item = request.form['item_index']
-    response = requests.post("http://server:5001/delete_item", json={'list_name': list_name, 'item': item})
+    response = requests.post("http://192.168.178.94:5001/delete_item", json={'list_name': list_name, 'item': item})
     return redirect(url_for('home', list=list_name))
 
 @app.route('/add_list', methods=['POST'])
 def add_list():
     list_name = request.form['list_name']
     new_list_name = request.form['new_list']
-    response = requests.post("http://server:5001/add_list", json={'list_name': list_name, 'new_list_name': new_list_name})
+    response = requests.post("http://192.168.178.94:5001/add_list", json={'list_name': list_name, 'new_list_name': new_list_name})
     return redirect(url_for('home', list=new_list_name))
 
 @app.route('/delete_list', methods=['POST'])
 def delete_list():
     list_name = request.form['list_name']
-    response = requests.post("http://server:5001/delete_list", json={'list_name': list_name})
+    response = requests.post("http://192.168.178.94:5001/delete_list", json={'list_name': list_name})
     return redirect(url_for('home', list='Einkaufszettel'))
 
 if __name__ == '__main__':
